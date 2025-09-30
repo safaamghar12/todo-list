101c7b1dd79d002c4b24e3ab29dad54441eb5798
safaa <safaamghar12@gmail.com>
2025-09-30 12:44:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const task = input.value.trim();
+
+  if (task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = () => li.remove(); // click = delete task
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+  }
+}
